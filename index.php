f131bc49af62b33eddc7170f8729b83beae7bb59
Andrés Javier López <ajavier.lopez@gmail.com>
2013-05-23 19:00:04 -0600

    Formulario básico

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-
-echo "hola mundo";
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Ejemplo de MoonDragon</title>
+	</head>
+	<body>
+		<h1>Formulario de prueba</h1>
+		<?php include "form.html"; ?>
+	</body>
+</html>
